8ecd0e3d122ce86cde88f8b7ff437082fe96e27f
Andrew Williams <awilliams@nextfaze.com>
2015-01-22 10:37:07 +1030

    Initial Commit

---
+++ NFRecord/NFRecord.h
@@ -0,0 +1,13 @@
+//
+//  NFRecord.h
+//  NFRecord
+//
+//  Created by Andrew Williams on 22/01/2015.
+//  Copyright (c) 2015 NextFaze. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NFRecord : NSObject
+
+@end
---
+++ NFRecord/NFRecord.m
@@ -0,0 +1,13 @@
+//
+//  NFRecord.m
+//  NFRecord
+//
+//  Created by Andrew Williams on 22/01/2015.
+//  Copyright (c) 2015 NextFaze. All rights reserved.
+//
+
+#import "NFRecord.h"
+
+@implementation NFRecord
+
+@end
